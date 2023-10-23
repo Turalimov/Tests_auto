1db1f68a02d5f155014b544aa917902a18d64c99
Turalimov <2ralimoff@gmail.com>
2023-10-23 18:51:51 +0600

    updated xpaths and asserts

--- test_login_swaglabs.py
+++ test_login_swaglabs.py
@@ -5,58 +5,61 @@ from selenium.webdriver.common.by import By
 def test_login_standard(browser):
     link = "https://www.saucedemo.com/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element(By.ID, "user-name").send_keys('standard_user')
-    browser.find_element(By.ID, "password").send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    title = browser.find_element(By.CLASS_NAME,'app_logo')
-    assert title.text == 'Swag Labs'
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('standard_user')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    url = browser.current_url
+    assert url == 'https://www.saucedemo.com/inventory.html'
 @pytest.mark.xfail
 def test_login_locked_out(browser):
     link = "https://www.saucedemo.com/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element(By.ID, "user-name").send_keys('locked_out_user')
-    browser.find_element(By.ID, "password").send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    title = browser.find_element(By.CLASS_NAME,'app_logo')
-    assert title.text == 'Swag Labs'
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('locked_out_user')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    url = browser.current_url
+    assert url == 'https://www.saucedemo.com/inventory.html'
+@pytest.mark.smoke
 def test_login_problem(browser):
     link = "https://www.saucedemo.com/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element(By.ID, "user-name").send_keys('problem_user')
-    browser.find_element(By.ID, "password").send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    title = browser.find_element(By.CLASS_NAME,'app_logo')
-    assert title.text == 'Swag Labs'
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('problem_user')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    url = browser.current_url
+    assert url == 'https://www.saucedemo.com/inventory.html'
 def test_login_glitch(browser):
     link = "https://www.saucedemo.com/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element(By.ID, "user-name").send_keys('performance_glitch_user')
-    browser.find_element(By.ID, "password").send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    title = browser.find_element(By.CLASS_NAME,'app_logo')
-    assert title.text == 'Swag Labs'
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('performance_glitch_user')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    url = browser.current_url
+    assert url == 'https://www.saucedemo.com/inventory.html'
 def test_login_error(browser):
     link = "https://www.saucedemo.com/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element(By.ID, "user-name").send_keys('error_user')
-    browser.find_element(By.ID, "password").send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    title = browser.find_element(By.CLASS_NAME,'app_logo')
-    assert title.text == 'Swag Labs'
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('error_user')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    url = browser.current_url
+    assert url == 'https://www.saucedemo.com/inventory.html'
 def test_login_visual(browser):
     link = "https://www.saucedemo.com/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element(By.ID, "user-name").send_keys('visual_user')
-    browser.find_element(By.ID, "password").send_keys('secret_sauce')
-    browser.find_element(By.ID, 'login-button').click()
-    title = browser.find_element(By.CLASS_NAME,'app_logo')
-    assert title.text == 'Swag Labs'
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('visual_user')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('secret_sauce')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    url = browser.current_url
+    assert url == 'https://www.saucedemo.com/inventory.html'
+def test_login_error(browser):
+    link = "https://www.saucedemo.com/"
+    browser.get(link)
+    browser.find_element('xpath', '//input[@id="user-name"]').send_keys('random')
+    browser.find_element('xpath', '//input[@id="password"]').send_keys('random_text')
+    browser.find_element('xpath', '//input[@id="login-button"]').click()
+    error = browser.find_element('xpath', '//h3[text()="Epic sadface: Username and password do not match any user in this service"]')
+    assert error.text == 'Epic sadface: Username and password do not match any user in this service'
 
 
 
